b3539261f893c93450148b7fe2bb089af03ec7f6
tommy huang <tommy.huang@wavenet.com.tw>
2021-01-11 14:24:26 +0800

    add 主畫面排版 & 素材 1. 新增 主畫面排版 2. 新增素材 3. 固定資料匯入

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
+import Home from './components/pages/HomePage/Home'
 
 function App() {
   return (
     <Router>
       <Navbar></Navbar>
       <Switch>
-        <Route path="/"></Route>
+        <Route path="/" exact component={Home} ></Route>
       </Switch>
     </Router>
   )
---
+++ src/components/pages/HomePage/Data.js
@@ -0,0 +1,12 @@
+export const homeObjOne = {
+  lightBg:false,
+  lightText:true,
+  lightTextDesc:true,
+  topLine: 'Exclusive Access',
+  headline: 'Unlimited Transactions with zero fees',
+  description:'Get access to our exclusive diamond card that allows you to send unlimited transactions without getting charged any fees',
+  buttonLabel:'開始吧',
+  imgStart:'',
+  img:'images/svg-1.svg',
+  alt:'Credit Card'
+}
