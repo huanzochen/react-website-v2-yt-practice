55dd9272267cf0ff10fc0d59237c3f4ad871278c
tommy.huang <tommy.huang@wavenet.com.tw>
2021-04-27 15:22:33 +0800

    add styled components

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,6 +2,70 @@ import React from 'react'
 import { Button } from './Button'
 import { Link } from 'react-router-dom'
 import './HeroSection.css'
+import styled from 'styled-components'
+import { device } from '../utils/device'
+
+const StyledHeroSection = styled.div`
+.home__hero-section {
+  color: #fff;
+  padding: 160px 0;
+}
+.darkBg {
+  background-color: #1c2237;
+}
+`
+
+const StyledTopLine = styled.div`
+color: #f00946;
+font-size: 16px;
+/* 行高 */
+line-height: 16px;
+/* 於 CSS1 中出現, 字體的粗度 400 是正常 700 相當於 bold  */
+font-weight: bold;
+/* 改變字元間的寬度 */
+letter-spacing: 1.4px;
+/* 強制大寫 */
+text-transform: uppercase;
+margin-bottom: 16px;
+`
+
+const StyledImg = styled.div`
+max-width: 555px;
+.home__hero-img {
+  max-width: 95%;
+  padding-right: 0;
+  margin: 0 0 0 10px;
+}
+`
+
+const StyledContainer = styled.div`
+.row {
+  display: flex;
+  margin-right: -15px;
+  margin-bottom: -15px;
+  margin-left: -15px;
+  flex-wrap: wrap;
+  align-content: stretch;
+}
+
+@media screen and ${device.mobile} {
+  padding-right: 30px;
+  padding-left: 30px;
+}
+
+@media screen and ${device.desktop} {
+  .home__hero-text-wrapper {
+    padding-bottom: 65px;
+  }
+  .col {
+    max-width: 100%;
+    flex-basis: 100%;
+  }
+}
+`
+
+
+
 
 function HeroSection({
   lightBg,
@@ -16,18 +80,19 @@ function HeroSection({
   imgStart
 }) {
   return (
-    <>
+    <StyledHeroSection>
       <div
         className={lightBg ? 'home__hero-section' : 'home__hero-section darkBg'}
       >
-        <div className='container'>
+        <StyledContainer>
+          {/* <div className='container'> */}
           <div className='row home__hero-row'
             style={{ display: 'flex',
               flexDirection: imgStart === 'start' ? 'row-reverse' : 'row' }}
           >
             <div className='col'>
               <div className='home__hero-text-wrapper'>
-                <div className='top-line'>{topLine}</div>
+                <StyledTopLine>{topLine}</StyledTopLine>
                 <h1 className={lightText ? 'heading' : 'heading dark'} > {headline}</h1>
                 <p className={lightTextDesc ? 'home__hero-subtitle' : 'home__hero-subtitle dark'}>
                   {description}
@@ -40,14 +105,14 @@ function HeroSection({
               </div>
             </div>
             <div className='col'>
-              <div className='home__hero-img-wrapper'>
+              <StyledImg>
                 <img src={img} alt={alt} className='home__hero-img' />
-              </div>
+              </StyledImg>
             </div>
           </div>
-        </div>
+        </StyledContainer>
       </div>
-    </>
+    </StyledHeroSection>
   )
 }
 
---
+++ src/utils/device.js
@@ -0,0 +1,13 @@
+// Mobile - 768px
+// Desktop - 991px
+
+
+const size = {
+  mobile: '768px',
+  desktop: '991px'
+}
+
+export const device = {
+  mobile:`(max-width: ${size.mobile})`,
+  desktop: `(max-width: ${size.desktop})`
+}
