1d2d19e1df22090514e33f95b4d611a43655620e
tommy.huang <tommy.huang@wavenet.com.tw>
2021-04-27 15:48:34 +0800

    move Herosection css to styled component

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -36,6 +36,14 @@ max-width: 555px;
   padding-right: 0;
   margin: 0 0 0 10px;
 }
+img {
+  border: 0;
+  max-width: 50%;
+
+  /* 用來指定行內元素與表格單元格的對齊方式 */
+  vertical-align: middle;
+  display: inline-block;
+}
 `
 
 const StyledContainer = styled.div`
@@ -48,9 +56,50 @@ const StyledContainer = styled.div`
   align-content: stretch;
 }
 
+.col {
+  margin-bottom: 15px;
+  padding-right: 15px;
+  padding-left: 15px;
+  flex-grow: 1;
+  max-width: 50%;
+  flex-basis: 50%;
+}
+
+.home__hero-row {
+  align-items: center;
+}
+
+.home__hero-text-wrapper {
+  max-width: 540px;
+  padding-top: 0;
+  padding-bottom: 60px;
+}
+
+.heading {
+  margin-bottom: 24px;
+  font-size: 48px;
+  line-height: 1.1;
+  font-weight: 600;
+  color: #f7f8fa;
+}
+
+.dark {
+  color: #1c2237;
+}
+
+.home__hero-subtitle {
+  /* 調整一句話的長度讓他變好看 */
+  max-width: 440px;
+  margin-bottom: 35px;
+  font-size: 18px;
+  line-height: 24px;
+}
+
 @media screen and ${device.mobile} {
-  padding-right: 30px;
-  padding-left: 30px;
+  .container{
+    padding-right: 30px;
+    padding-left: 30px;
+  }
 }
 
 @media screen and ${device.desktop} {
@@ -85,29 +134,30 @@ function HeroSection({
         className={lightBg ? 'home__hero-section' : 'home__hero-section darkBg'}
       >
         <StyledContainer>
-          {/* <div className='container'> */}
-          <div className='row home__hero-row'
-            style={{ display: 'flex',
-              flexDirection: imgStart === 'start' ? 'row-reverse' : 'row' }}
-          >
-            <div className='col'>
-              <div className='home__hero-text-wrapper'>
-                <StyledTopLine>{topLine}</StyledTopLine>
-                <h1 className={lightText ? 'heading' : 'heading dark'} > {headline}</h1>
-                <p className={lightTextDesc ? 'home__hero-subtitle' : 'home__hero-subtitle dark'}>
-                  {description}
-                </p>
-                <Link to="/sign-up">
-                  <Button buttonSize='btn--wide' buttonColor='blue'>
-                    {buttonLabel}
-                  </Button>
-                </Link>
+          <div className='container'>
+            <div className='row home__hero-row'
+              style={{ display: 'flex',
+                flexDirection: imgStart === 'start' ? 'row-reverse' : 'row' }}
+            >
+              <div className='col'>
+                <div className='home__hero-text-wrapper'>
+                  <StyledTopLine>{topLine}</StyledTopLine>
+                  <h1 className={lightText ? 'heading' : 'heading dark'} > {headline}</h1>
+                  <p className={lightTextDesc ? 'home__hero-subtitle' : 'home__hero-subtitle dark'}>
+                    {description}
+                  </p>
+                  <Link to="/sign-up">
+                    <Button buttonSize='btn--wide' buttonColor='blue'>
+                      {buttonLabel}
+                    </Button>
+                  </Link>
+                </div>
               </div>
-            </div>
-            <div className='col'>
-              <StyledImg>
-                <img src={img} alt={alt} className='home__hero-img' />
-              </StyledImg>
+              <div className='col'>
+                <StyledImg>
+                  <img src={img} alt={alt} className='home__hero-img' />
+                </StyledImg>
+              </div>
             </div>
           </div>
         </StyledContainer>
